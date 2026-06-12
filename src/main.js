387d10f339e64a7bd35017a612052609243f13de
Vladislav Obukhov <vdaobukhov8@gmail.com>
2026-06-12 17:31:17 +0300

    Complete responsive Bose landing page

--- src/main.js
+++ src/main.js
@@ -2,9 +2,8 @@ const phoneButton = document.querySelector('.header__phone');
 const phoneNumber = document.querySelector('.header__phone-number');
 
 if (phoneButton && phoneNumber) {
-  phoneButton.addEventListener('click', event => {
+  phoneButton.addEventListener('click', (event) => {
     event.preventDefault();
-
     phoneNumber.classList.toggle('active');
   });
 }
@@ -17,15 +16,18 @@ const menuLinks = document.querySelectorAll('.menu__nav a');
 if (menuOpen && menuClose && menu) {
   menuOpen.addEventListener('click', () => {
     menu.classList.add('active');
+    document.body.style.overflow = 'hidden';
   });
 
   menuClose.addEventListener('click', () => {
     menu.classList.remove('active');
+    document.body.style.overflow = '';
   });
 
-  menuLinks.forEach(link => {
+  menuLinks.forEach((link) => {
     link.addEventListener('click', () => {
       menu.classList.remove('active');
+      document.body.style.overflow = '';
     });
   });
 }
